712491d613d104a5b04fc222bbf87481ed0822e0
Bryce Beeston <B.Beeston@netsupportsoftware.com>
2021-04-22 11:42:23 +0100

    Initial dialog.

--- PhotoManager/Form1.Designer.cs
+++ PhotoManager/Form1.Designer.cs
@@ -29,13 +29,121 @@ namespace PhotoManager
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListView();
+            this.Filename = new System.Windows.Forms.ColumnHeader();
+            this.Path = new System.Windows.Forms.ColumnHeader();
+            this.Date = new System.Windows.Forms.ColumnHeader();
+            this.Size = new System.Windows.Forms.ColumnHeader();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(13, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(405, 23);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(424, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(527, 12);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 2;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Filename,
+            this.Path,
+            this.Date,
+            this.Size});
+            this.lstFiles.FullRowSelect = true;
+            this.lstFiles.GridLines = true;
+            this.lstFiles.HideSelection = false;
+            this.lstFiles.Location = new System.Drawing.Point(13, 43);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(775, 370);
+            this.lstFiles.TabIndex = 3;
+            this.lstFiles.UseCompatibleStateImageBehavior = false;
+            this.lstFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // Filename
+            // 
+            this.Filename.Text = "Filename";
+            this.Filename.Width = 125;
+            // 
+            // Path
+            // 
+            this.Path.Text = "Path";
+            this.Path.Width = 400;
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            this.Date.Width = 128;
+            // 
+            // Size
+            // 
+            this.Size.Text = "Size";
+            this.Size.Width = 100;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.Location = new System.Drawing.Point(1, 424);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(798, 23);
+            this.txtStatus.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.ListView lstFiles;
+        private System.Windows.Forms.ColumnHeader Path;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.ColumnHeader Size;
+        private System.Windows.Forms.ColumnHeader Filename;
+        private System.Windows.Forms.TextBox txtStatus;
     }
 }
 
--- PhotoManager/Form1.cs
+++ PhotoManager/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,66 @@ namespace PhotoManager
 {
     public partial class Form1 : Form
     {
+        private List<string> listPhotos;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = folderBrowser.SelectedPath;
+            }
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            listPhotos = new List<string> { };
+
+            Recurse(txtPath.Text);
+        }
+
+        private void Recurse(string path)
+        {
+            try
+            {
+                EnumerationOptions enumerationOptions = new EnumerationOptions();
+                enumerationOptions.RecurseSubdirectories = true;
+
+                foreach (var f in Directory.GetFiles(path, "*.jpg", enumerationOptions))
+                {
+                    listPhotos.Add(f);
+                }
+
+                foreach (var f in Directory.GetFiles(path, "*.jpeg", enumerationOptions))
+                {
+                    listPhotos.Add(f);
+                }
+                
+                listPhotos.Sort();
+
+                lstFiles.Items.Clear();
+
+                foreach(var photo in listPhotos)
+                {
+                    FileInfo fi = new FileInfo(photo);
+
+                    ListViewItem item = new ListViewItem();
+                    string[] details = { fi.DirectoryName, fi.CreationTime.ToString(), fi.Length.ToString() };
+                    lstFiles.Items.Add(fi.Name).SubItems.AddRange(details);
+                }
+
+                txtStatus.Text = $"{listPhotos.Count} files found.";
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
     }
 }
